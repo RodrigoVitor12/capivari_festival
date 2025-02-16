0515ef1314c39a95dd1cc7f21a01db99d44602c2
RodrigoVitor <rodrigovitordev2024@gmail.com>
2025-02-16 08:43:07 -0300

    Alterando imagem de fundo no mobile

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,9 @@
 export default function Main () {
     return (
     <main>
-        <div className="bg-[url('./assets/bg-image.jpg')] bg-cover bg-center min-h-120 md:min-h-150 flex flex-col justify-center p-4" >
+        <div 
+          className="bg-[url('./assets/bg-mobile.png')] md:bg-[url('./assets/bg-image.jpg')] bg-cover bg-center min-h-120 md:min-h-150 flex flex-col justify-center p-4" 
+        >
           <h1 className="text-white text-5xl md:text-8xl  font-bold text-center">Capivari Festival</h1>
           <p className="text-white text-center text-xl mt-4  font-light">08 de fevereiro de 2025 | Jardim Secreto, Santa Rita do Sapucai - MG</p>
 
